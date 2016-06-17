8639af270d87762f72eae0e4d38e80b07dff0095
Nigel Rahkola <me@nigelrahkola.com>
2016-06-17 11:18:55 -0700

    Download the rest of the pages
    Handle this iteratively, and generally. This code assumes that the
    api will consistently return the amount of transactions found in
    the first page if it can.

--- restTest.js
+++ restTest.js
@@ -1,9 +1,36 @@
 var rp = require('request-promise'),
     _ = require('lodash');
 
-rp('http://resttest.bench.co/transactions/1.json')
-    .then(function (res) {
-        console.log(res);
-    }).catch(function (err) {
-        console.log(err);
-    });
+
+function getTransactions(page) {
+    var options = {
+        uri: 'http://resttest.bench.co/transactions/' + page + '.json',
+        json: true
+    };
+    return rp(options);
+}
+
+function main() {
+    var transactions = [];
+    getTransactions(1)
+        .then(function (res) {
+            transactions = _.concat(transactions, res.transactions);
+            var totalCount = res.totalCount;
+            var page = res.page;
+            var pagesLeft = _.ceil((totalCount - transactions.length) / transactions.length);
+            // console.log('pagesLeft: ' + pagesLeft);
+            var promises = [];
+            for(var i = page + 1; i <= page + pagesLeft; i++) {
+                promises.push(getTransactions(i));
+            }
+            return Promise.all(promises);
+        }).then(function(things) {
+            _.each(things, function(res) {
+                transactions = _.concat(transactions, res.transactions);
+            });
+        }).catch(function (err) {
+            console.log(err);
+        });
+}
+
+main();
