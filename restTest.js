68765485a81e40b65a5592aa4ef82d7e68000a78
Nigel Rahkola <me@nigelrahkola.com>
2016-06-17 14:48:03 -0700

    Remove 'main' function and call from the module
    This functionality should probably be made into a module and
    exported eventually anyway.

--- restTest.js
+++ restTest.js
@@ -26,36 +26,35 @@ function deepCopy(obj) {
     return JSON.parse(JSON.stringify(obj));
 }
 
-function main() {
-    var transactions = [];
-    getTransactions(1)
-        .then(function (res) {
+var transactions = [];
+getTransactions(1)
+    .then(function (res) {
+        transactions = _.concat(transactions, res.transactions);
+        var totalCount = res.totalCount;
+        var page = res.page;
+        var pagesLeft = _.ceil((totalCount - transactions.length) / transactions.length);
+        // console.log('pagesLeft: ' + pagesLeft);
+        var promises = [];
+        for(var i = page + 1; i <= page + pagesLeft; i++) {
+            promises.push(getTransactions(i));
+        }
+        return Promise.all(promises);
+    }).then(function(things) {
+        _.each(things, function(res) {
             transactions = _.concat(transactions, res.transactions);
-            var totalCount = res.totalCount;
-            var page = res.page;
-            var pagesLeft = _.ceil((totalCount - transactions.length) / transactions.length);
-            // console.log('pagesLeft: ' + pagesLeft);
-            var promises = [];
-            for(var i = page + 1; i <= page + pagesLeft; i++) {
-                promises.push(getTransactions(i));
-            }
-            return Promise.all(promises);
-        }).then(function(things) {
-            _.each(things, function(res) {
-                transactions = _.concat(transactions, res.transactions);
-            });
-            var totalBalance = calculateTotalBalance(transactions);  // Crunch the numbers
-            console.log('total balance is ' + totalBalance);
-
-            humanizedTransactions = _.map(transactions, function(t) {
-                var t2 = deepCopy(t);
-                t2.HumanizedCompany = humanizeVendorName(t2.Company);
-                return t2;
-            });
-
-        }).catch(function (err) {
-            console.log(err);
         });
-}
+        console.log('# of transactions is ' + transactions.length);
 
-main();
+        var totalBalance = calculateTotalBalance(transactions);  // Crunch the numbers
+        console.log('total balance is ' + totalBalance);
+
+        humanizedTransactions = _.map(transactions, function(t) {
+            var t2 = deepCopy(t);
+            t2.HumanizedCompany = humanizeVendorName(t2.Company);
+            return t2;
+        });
+        // console.log(humanizedTransactions);
+
+    }).catch(function (err) {
+        console.log(err);
+    });
